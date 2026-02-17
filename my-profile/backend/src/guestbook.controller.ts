8193349551badb9a8b216310d79abb2885c6a573
Gwyneth Salud <gcsalud@student.apc.edu.ph>
2026-02-17 15:26:39 +0800

    Added engines to json package

--- my-profile/backend/src/guestbook.controller.ts
+++ my-profile/backend/src/guestbook.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { GuestbookService } from './guestbook.service';
 
-@Controller('guestbook')
+@Controller('api/guestbook')
 export class GuestbookController {
   constructor(private readonly service: GuestbookService) {}
 
